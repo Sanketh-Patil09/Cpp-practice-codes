32f7dd210b04235bf70766073ac533d874b8b43d
Sanketh-Patil09 <sanketh.patil09@gmail.com>
2024-02-26 12:26:00 +0530

    new changes

---
+++ functions.cpp
@@ -0,0 +1,36 @@
+/*Write a function int max_of_four(int a, int b, int c, int d) which reads four arguments and returns the greatest of them.
+Note
+There is not built in max function in C. Code that will be reused is often put in a separate function, e.g. int max(x, y) that returns the greater of the two values.*/
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int max_of_four(int a, int b, int c, int d)
+{
+    int max;
+    if(a>b&&a>d&&a>c)
+    {
+        max=a;
+    }
+    else if(a<b&&b>d&&b>c)
+    {
+        max=b;
+    } 
+    else if(c>b&&c>d&&a<c)
+    {
+        max=c;
+    }
+    else if(d>b&&a<d&&d>c)
+    {
+        max=d;
+    }
+    return max;
+}
+
+int main() 
+{
+    int a, b, c, d;
+    cin>>a>>b>>c>>d;
+    int ans = max_of_four(a, b, c, d);
+    cout<<ans<<endl;
+    return 0;
+}
